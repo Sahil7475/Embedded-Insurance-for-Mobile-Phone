bf705a422b2a177c0ea8dc6a739f5fdd6828eff7
Sahil <Sahil7475@gmail.com>
2023-08-15 17:02:24 +0530

    final commit

--- apk.py
+++ apk.py
@@ -1,6 +1,6 @@
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 from flask_login import current_user
-from flask import Flask,session,render_template,request,redirect,jsonify,url_for
+from flask import Flask,session,render_template,request,redirect,jsonify,url_for,render_template_string
 from google.cloud import firestore
 from flask import request
 import firebase_admin
@@ -13,19 +13,30 @@ from flask_admin import Admin,expose
 from flask_admin.contrib.pymongo import ModelView
 from flask_admin import AdminIndexView
 from flask_bcrypt import Bcrypt
-
+from flask_mail import Mail, Message
 
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'embeddedinsurance@gmail.com'
+app.config['MAIL_PASSWORD'] = 'wbqmsgclyhsdygqj'
 app.secret_key = 'sahil'
 bcrypt = Bcrypt(app)
-
-cred = credentials.Certificate(r'C:\Users\HP\OneDrive\Desktop\web development\DEEPBLUE\EIM-Embbedded Insurance for mobile phone\test-f38d8-firebase-adminsdk-ag3ap-0f571452d9.json')
+mail = Mail(app)
+cred = credentials.Certificate(r'C:\Users\HP\OneDrive\Desktop\web development\DEEPBLUE\EIM-Embbedded Insurance for mobile phone\insurance-claimer-36ec2-firebase-adminsdk-bwg6f-473f77c3cb.json')
 firebase_admin.initialize_app(cred,{
-    'projectId':'test-f38d8',
-    'storageBucket': 'test-f38d8.appspot.com',
-    'databaseURL': 'https://test-f38d8-default-rtdb.firebaseio.com/'
-})
+    'projectId':'insurance-claimer-36ec2',
+    'storageBucket': 'insurance-claimer-36ec2.appspot.com',
+    'databaseURL': 'https://insurance-claimer-36ec2-default-rtdb.firebaseio.com/'
+}) 
+
+rf=db.reference('Mobile1') 
+
+
 
 
 dab = firestore.client()
@@ -35,6 +46,8 @@ firebase_storage = storage.bucket()
 
 login_manager = LoginManager(app)
 login_manager.init_app(app)
+
+
 
 
 class User(UserMixin):
@@ -58,31 +71,11 @@ def load_user(user_id):
     user = User()
     user.id = user_ref.id
     return user
-""" 
-@login_manager.user_loader
-def load_user(user_id):
-    user_ref = dab.collection('user').document(user_id).get()
-    if not user_ref.exists:
-        return None
 
-    user = User()
-    user.id = user_ref.id
-    return user
-
-    
-
-@login_manager.user_loader
-def load_user(user_id):
-    admin_ref = dab.collection('admin').document(user_id).get()
-    if admin_ref.exists:
-        admin = Admin()
-        admin.id = admin_ref.id
-        return admin
-    return None """
 
 @app.route('/',methods=["POST","GET"])
 def home():
-    return redirect(url_for('login'))
+    return render_template('home.html')
 
 
 
@@ -94,37 +87,56 @@ def signup():
         name=request.form.get('name')
         password1=request.form.get('password1')
         password2=request.form.get('password2')
-        file = request.files['file']
+        address=request.form.get('address')
+        type=request.form.get('type')
+        policynumber=request.form.get('policynumber')
+        phoneno=request.form.get('phoneno')
+        company=request.form.get('company')
+
+        
 
 
         if password1 == password2:
             
         # Check if user already exists
             user_ref = dab.collection('user').where('email', '==', email).get()
+            user_policy = dab.collection('user').where('policynumber', '==',policynumber).get()
             if len(user_ref) > 0:
-                return {'message': 'User already exists'}, 409
-
+                error_message = 'User already exists'
+                return render_template('register.html', error_message=error_message), 404
+            
+            if len(user_policy) > 0:
+                error_message = 'Policy Number Already exists'
+                return render_template('register.html',error_message=error_message),404
+            
             hashed_password = bcrypt.generate_password_hash(password1).decode('utf-8')
 
-            filename = f'user_{request.form["name"]}_{file.filename}'
-            blob = firebase_storage.blob(filename)
-            blob.upload_from_file(file)
-
-             # Get the download URL of the uploaded file
-            download_url = blob.public_url
-            # Create new user document
+            
             new_user_ref = dab.collection('user').document()
             new_user_ref.set({
                 'email': email,
                 'password': hashed_password,
                 'name':name,
-                'file':download_url,
-                
+                'address':address,
+                'type':type,
+                'phoneno':phoneno,
+                'policynumber':policynumber, 
+                'company':company,
+                'amount':1440              
             })
+
+            ref = db.reference(type)
+            realtime_data = ref.get()
+            if realtime_data['Backside'] or realtime_data['Bottomleftcorner'] or realtime_data['Bottomrightcorner'] or realtime_data['Topleftcorner'] or realtime_data['Toprightcorner'] > 11:
+                subject = 'Your Phone is broken'
+                body = 'Dear ' + new_user_ref.get().to_dict()['name'] +' ,\n\nWe are sorry to hear that your phone is broken. To manage your account with us, please log in to our website at http://127.0.0.1:5000/. Our website is mobile-friendly and can be accessed from any web browser.\n\nThank you for choosing our company for your insurance needs.\n\nBest regards,\n\nCOVERWELL .'
+                send_email(email, subject, body)
+
             return render_template('login.html')
 
         else:
-            return {'message':'Password not matched'},409
+            error_message = 'Password not matched'
+            return render_template('register.html', error_message=error_message), 404
 
 
     return render_template('register.html')
@@ -139,7 +151,8 @@ def login():
         user_ref = dab.collection('user').where('email', '==', email).get()
 
         if len(user_ref) == 0:
-            return {'message': 'User not found'}, 404
+            error_message = 'User not found'
+            return render_template('login.html', error_message=error_message), 404
 
         user = user_ref[0].to_dict()
         user_doc = user_ref[0].reference
@@ -158,15 +171,33 @@ def login():
             print(message_data)
 
         if not bcrypt.check_password_hash(user['password'], password):
-            return {'message': 'Incorrect password'}, 401
+            error_message = 'Incorrect Password'
+            return render_template('login.html', error_message=error_message), 401
 
+        for doc in user_ref:
+        # Get the data of the document as a dictionary
+            user_data = doc.to_dict()
+
+        if len(user_ref) == 1:
+            # Get the value of the 'type' field from the user data dictionary
+            user_type = user_data.get('type')
+            print(user_type)
+
+        ref = db.reference(user_type)
+
+        # Query the data from Realtime Database
+        realtime_data = ref.get()
+        print(realtime_data)
         print(email)
         print(password)
         user_obj = User()
         user_obj.id = user_ref[0].id
-
+        if realtime_data['Backside'] or realtime_data['Bottomleftcorner'] or realtime_data['Bottomrightcorner'] or realtime_data['Topleftcorner'] or realtime_data['Toprightcorner'] > 11:
+            subject = 'Your Phone is broken'
+            body = 'Dear ' + user['name'] + ' ,\n\nWe are sorry to hear that your phone is broken. To manage your account with us, please log in to our website at http://127.0.0.1:5000/. Our website is mobile-friendly and can be accessed from any web browser.\n\nThank you for choosing our company for your insurance needs.\n\nBest regards,\n\nCOVERWELL .'
+            send_email(email, subject, body)
         login_user(user_obj)
-        return render_template('data.html', users=user, message=message_data, data=data)
+        return render_template('data.html', users=user, message=message_data, data=data,realtime_data=realtime_data)
 
     return render_template('login.html')
 
@@ -194,13 +225,14 @@ def adminsignup():
         name = request.form.get('name')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-       
+        phoneno=request.form.get('phoneno')
 
         if password1 == password2:
             # Check if admin already exists
             admin_ref = dab.collection('admin').where('email', '==', email).get()
             if len(admin_ref) > 0:
-                return {'message': 'Admin already exists'}, 409
+                error_message = 'Admin Already exists'
+                return render_template('admin-register.html', error_message=error_message), 401
 
             hashed_password = bcrypt.generate_password_hash(password1).decode('utf-8')
 
@@ -210,14 +242,14 @@ def adminsignup():
                 'email': email,
                 'password': hashed_password,
                 'name': name,
-               
+                'phoneno':phoneno,
             })
 
             return render_template('admin-login.html')
 
         else:
-            return {'message': 'Password not matched'}, 409
-
+            error_message = 'Password not matched'
+            return render_template('admin-register.html', error_message=error_message), 409
     return render_template('admin-register.html')
 
 @app.route('/admin-login', methods=["POST","GET"])
@@ -230,7 +262,8 @@ def admin_login():
         # Check if admin exists
         admin_ref = dab.collection('admin').where('email', '==', email).get()
         if len(admin_ref) == 0:
-            return {'message': 'Admin not found'}, 401
+            error_message = 'Company not found'
+            return render_template('admin-login.html', error_message=error_message), 401
 
         admin_data = admin_ref[0].to_dict()
 
@@ -241,7 +274,8 @@ def admin_login():
             login_user(admin_obj)
             return redirect(url_for('admin_dashboard'))
         else:
-            return {'message': 'Invalid password'}, 401
+            error_message = 'Invalid password'
+            return render_template('admin-login.html', error_message=error_message), 401
 
     return render_template('admin-login.html')
 
@@ -294,9 +328,31 @@ def admin_dashboard():
     users = []
     for user_doc in user_ref:
         user_data = user_doc.to_dict()
-        users.append(user_data)
+        if user_data.get('company') == admin['name']:
+            users.append(user_data)
+        
 
-    return render_template('admin-dashboard.html', users=users)
+    for user in users:
+        user_type = user["type"]
+        print(user_type)
+    
+    # Construct the reference path based on the user type
+        ref_path = f"/{user_type}"  # replace with the correct path
+
+     # Get the reference to the database node
+        ref = db.reference(ref_path)
+
+    # Query the data from Realtime Database
+        realtime_data = ref.get()
+
+        print(realtime_data)
+    
+    yesalert="yes"
+    noalert="no"
+    
+
+    
+    return render_template('admin-dashboard.html', users=users,realtime_data=realtime_data,yes=yesalert,no=noalert,admin=admin)
 
 
 @app.route('/send-data', methods=["POST", "GET"])
@@ -348,6 +404,7 @@ def showdata():
         if not email:
             return "No email provided", 400
         user_ref = dab.collection('user').where('email', '==', email).get()
+        user = user_ref[0].to_dict()
         for doc in user_ref:
         # Get the data of the document as a dictionary
             user_data = doc.to_dict()
@@ -361,99 +418,42 @@ def showdata():
         # Query the data from Realtime Database
         realtime_data = ref.get()
         print(realtime_data)
+        if realtime_data['Backside'] or realtime_data['Bottomleftcorner'] or realtime_data['Bottomrightcorner'] or realtime_data['Topleftcorner'] or realtime_data['Toprightcorner'] > 11:
+                subject = 'Your Phone is broken'
+                body = 'Dear ' + user['name'] + ' ,\n\nWe are sorry to hear that your phone is broken. To manage your account with us, please log in to our website at http://127.0.0.1:5000/. Our website is mobile-friendly and can be accessed from any web browser.\n\nThank you for choosing our company for your insurance needs.\n\nBest regards,\n\nCOVERWELL .'
+                send_email(email, subject, body)
+
         if not user_ref:
             return "User not found", 404
         user_doc = user_ref[0].reference
-        data_ref = user_doc.collection('data').get()
-        data = {}
-        if data_ref:
-            data = data_ref[0].to_dict()
-        return render_template('showdata.html', data=realtime_data)
+        amount_ref = user_doc.collection('amount').get()
+        amount = {}
+        if amount_ref:
+            amount = amount_ref[0].to_dict()
+        return render_template('showdata.html', data=realtime_data,user=user)
     else:
         return "Method Not Allowed", 405
 
 
 
-""" @app.route('/show')
-
-def show():
-    dbe = firestore.client()
-    user_type = dbe.collection('user').get('type')
-    # Get a reference to the Realtime Database
-    ref = db.reference(user_type)
-
-# Query the data from Realtime Database
-    realtime_data = ref.get()
-
-# Get a reference to the Firestore database
-    
-
-# Query the data from Firestore
-    firestore_data = dbe.collection('user').get()
-    
-# Merge the data from both sources
-    merged_data = {}
-    for doc in firestore_data:
-        user_id = doc.id
-        merged_data[user_id] = {}
-        merged_data[user_id].update(realtime_data.get(user_id, {}))
-        merged_data[user_id].update(doc.to_dict())
-
-    user_ref = dab.collection('user').get()
-    user_doc = user_ref[0].reference
-    data_ref = user_doc.collection('data').document()
-
-    
-
-# Store the merged data in Firestore
-    for user_id in merged_data:
-        data_ref.set(
-        realtime_data
-    )
-
-# Return a success message
-    return 'Data merged and stored in Firestore'
-    
-
-# Define a route for merging data from both databases
-@app.route('/merge-data')
-def merge_data():
-    # Retrieve data from Realtime Database
-    
-
-    realtime_ref = db.reference('Mobile1')
-    realtime_data = realtime_ref.get()
-    print(realtime_data)
-    # Retrieve data from Firestore
-    firestore_ref = firestore.client().collection('user')
-    firestore_data = {doc.id: doc.to_dict() for doc in firestore_ref.stream()}
-
-    # Merge the data
-    merged_data = {}
-    for user_id in realtime_data:
-        merged_data[user_id] = {}
-        merged_data[user_id].update(dict(realtime_data[user_id]))
-        merged_data[user_id].update(firestore_data.get(user_id, {}))
-
-    # Return the merged data as a JSON response
-    return jsonify(merged_data)
-
- """
 
 
+def send_email(recipient, subject, body):
+    msg = Message(subject=subject, recipients=[recipient], body=body, sender='embeddedinsurance@gmail.com')
+    mail.send(msg)
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return render_template('admin-login.html')
+    return render_template('home.html')
 
 @app.route('/logout-user')
 @login_required
 def logoutuser():
     if current_user.is_authenticated:
         logout_user()
-    return render_template('login.html')
+    return render_template('home.html')
 
 @app.route('/protected')
 @login_required
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,4 @@
-/adminsignup
+
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
